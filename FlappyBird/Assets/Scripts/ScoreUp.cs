cd6f9ff27bd1a0b414aa822c5faca50d695f0ac8
jintg0821 <jintg0825@gmail.com>
2026-01-13 14:59:23 +0900

    [Fix] 배경, 점수 수정

--- FlappyBird/Assets/Scripts/ScoreUp.cs
+++ FlappyBird/Assets/Scripts/ScoreUp.cs
@@ -6,6 +6,8 @@ public class ScoreUp : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        BirdJump player = collision.GetComponent<BirdJump>();
+        if (player.isDie) return;
         Score.score++;
     }
 }
